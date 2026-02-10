53e0fa407c9c50c662629ceeeae45dbd51f7c6ed
GolanTrevize10 <25170481+GolanTrevize10@users.noreply.github.com>
2026-02-10 10:40:17 +0100

    Update InsecureConfig.java
    Signed-off-by: GolanTrevize10 <25170481+GolanTrevize10@users.noreply.github.com>

--- src/main/java/org/springframework/samples/petclinic/InsecureConfig.java
+++ src/main/java/org/springframework/samples/petclinic/InsecureConfig.java
@@ -1,3 +1,10 @@
-public class InsecureConfig {
+package org.springframework.samples.petclinic;
+
+public final class InsecureConfig {
+
     private static final String DB_PASSWORD = "password123";
+
+    private InsecureConfig() {
+    }
+
 }
